2462c439aa54ea310b603f19521abb503b127770
Kosyura Olga <olyakosyura@gmail.com>
2015-05-11 14:29:05 +0300

    Рубежный контроль 3

---
+++ ИУ7-27_ВАРИАНТ-08_КОСЮРА_РК3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+            string code;
+            int last;
+            last = lines.Length - 1;
+            code = "";
+            for (int i = 0; i < lines[last].Length; i++)
+            {
+
+                if (char.IsUpper(lines[last],i)==true)
+                {
+                    for (int j = 0; j < lines.Length - 1; j++)
+                    {
+                        if (lines[last][i] == lines[j][0])
+                            code = code + lines[j].Remove(0, 2) ;
+                    }
+
+                }
+                else
+                code += lines[last][i];
+            }
+            for (int i = 0; i < code.Length-1; i++)
+            {
+                if (code[i] == '.')
+                {
+                    code=code.Remove(i - 1, 2);
+                }
+            }
+
+
+            System.Console.WriteLine(code);
+            System.Console.WriteLine("result:");
+            string t="";
+            for (int i = 0; i < code.Length - 1; i++)
+            {
+                if (char.IsSymbol(code[i]) == true)
+                {
+                    for (int j = i; j < code.Length - 1; j++)
+                    {
+                        t += code[i];
+                    }
+                }
+                else 
+                    t = t+code[i];
+
+            }
+            code =t;
+            System.Console.WriteLine(code);
+            System.Console.ReadLine();
+        }
+    }
+}
